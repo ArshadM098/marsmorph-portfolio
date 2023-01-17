e359f69bb74b6fbd2aea54724f67003a744d6f1b
ArshadM098 <marsha6@uic.edu>
2023-01-17 17:18:21 -0600

    Larger Screen Responsive Update for Index

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,8 @@ export default function NavBar({children}){
     return <>
         <div className="fixed flex justify-around
          border-solid border-white border
-         p-0 px-7 mx-3 rounded overflow-hidden min-h-[65px] bg-dark z-10 place-self-center">
+         p-0 px-7 mx-3 rounded overflow-hidden min-h-[65px] bg-dark z-10 self-center
+         sm:self-start sm:ml-5">
             <div className=" m-3 mr-0 text-2xl lowercase text-accent">| </div>
             <NavBtns href="/">Home</NavBtns>
             <NavBtns href="/featuredProject">Projects</NavBtns>
