78d7d758ea75ceeda6e5d1d10813e22d08b847a3
ArshadM098 <marsha6@uic.edu>
2023-01-17 12:12:02 -0600

    Reverted to orignal method for desc view

--- pages/projectMenu.js
+++ pages/projectMenu.js
@@ -1,34 +1,17 @@
 
 import NavBar from '../components/navbar';
 import SkillTree from '../components/skilltree';
-import { getPostData,getAllPostIds } from '../lib/blogs.js';
+import { getPostData,getAllPostIds, getSortedPostsData } from '../lib/blogs.js';
 import ProjectPreview from '../components/projectPreview';
 
 export  function getStaticProps() {
 
-  const ids =  getAllPostIds();
-  console.log(ids)
-  
-
-  const allPostsData = ids.map(  ({params})=>{
-    
-    const data =  getPostData(params.id);
-    console.log(data);
-    const id = params.id
-    const desc = data.description;
-    const title = data.title;
-    return{
-      id,
-      desc,
-      title,
-    }})
+  const allPostsData =  getSortedPostsData();
   return{
     props:{
       allPostsData
     }
 }}
-
-
 
 export default function ProjectMenu({allPostsData}){
 
@@ -36,19 +19,11 @@ export default function ProjectMenu({allPostsData}){
   <div className="container flex flex-col text-white h-full">
     <NavBar/>
     <ul className="list-none  mt-24 mx-3">
-    {allPostsData.map(({ id,title,desc}) => (
-      <li key={id} ><ProjectPreview title={title} desc={desc}/></li>
+    {allPostsData.map(({ id,title,description}) => (
+      <li key={id} ><ProjectPreview title={title} desc={description}/></li>
     ))}
     </ul> 
   </div>
   )
 }
-{/* */}
-// }
-// export default function ProjectMenu({allPostsData}){
-//   return (
-//     <Layout >
-//       <Menu allPostsData={allPostsData}></Menu>
-//     </Layout>
-//   )
 
--- pages/projects/blogpages/[id].js
+++ pages/projects/blogpages/[id].js
@@ -11,7 +11,6 @@ export async function getStaticProps({ params }) {
       },
     };
   }
-
 export async function getStaticPaths(){
     const paths = getAllPostIds();
     return {
