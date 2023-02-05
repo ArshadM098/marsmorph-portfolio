6ebae1ffc64e2d59b63e7ad5e69d0aec512a2d19
ArshadM098 <marsha6@uic.edu>
2023-02-05 17:19:53 -0600

    Changed sidebar background

--- components/sidebar.js
+++ components/sidebar.js
@@ -14,7 +14,7 @@ export default function Sidebar(){
         onClick={() => setShowDropdown(!showDropdown)}
         src={PFbtn}
         className="absolute object-contain right-0 m-4 h-8 w-8 z-40 border-none"/>
-        <div className={`absolute bg-black flex flex-col items-center z-30 border-0 border-t-4 border-solid border-accent
+        <div className={`absolute backdrop-brightness-[0.25] backdrop-blur-md flex flex-col items-center z-30 border-0 border-t-4 border-solid border-accent
         bottom-0 py-2 w-full h-3/5 transform origin-top-right ${showDropdown ? 'translate-y-0' : 'translate-y-full'} transition-all`}>
           <Image onClick={() => setShowDropdown(false)} src={Exit} className="active:bg-accent object-contain self-end mr-4 mt-2 m-0 w-8"/>
           <Image src={PFP} className="object-contain w-3/5 mt-0"/>
